16fcbb05a6934af36100d52075c17159a220b78e
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-09-12 09:39:17 -0700

    update: Added Discord whitelist options

--- config.lua
+++ config.lua
@@ -1,12 +1,18 @@
--- WIP (Maybe add more later)
+-----------------For support, scripts, and more----------------
+----------------- https://discord.gg/XJFNyMy3Bv ---------------
+---------------------------------------------------------------
 Config = {}
 
 Config.DiscordInfo = {
-    botToken = 'DISCORD_BOT_TOKEN',
-    serverId = 'DISCORD_SERVER_ID',
+    botToken = 'DISCORD_BOT_TOKEN', -- Your Discord bot token here
+    serverId = 'DISCORD_SERVER_ID', -- Your Discord's server ID here(Aka Guild ID)
 }
 
-
-
-
-
+Config.DiscordWhitelist = { -- Restrict if someone can fly in if they lack specific Discord role(s)
+    enabled = true, -- Enable?
+    deniedMessage = 'https://discord.gg/jsd : Join our Discord server and verify to play!', -- Message for those who lack whitelisted role(s)
+    whitelistedRoles = {
+      --'ROLE_ID_HERE',  
+        '835370971501297706', -- Maybe like a civilian role or whitelisted role(can add multiple to table)
+    }
+}
