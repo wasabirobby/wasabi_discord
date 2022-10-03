29e9918c5a266efc4a228100823b0000d8ac2859
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-10-03 14:00:01 -0700

    chore: moved functions to own file

---
+++ client/functions.lua
@@ -0,0 +1,21 @@
+-----------------For support, scripts, and more----------------
+--------------- https://discord.gg/wasabiscripts  -------------
+---------------------------------------------------------------
+
+checkForRole = function(roleId)
+    local hasPerms = lib.callback.await('ws_discord:checkForRole', 100, roleId)
+    return hasPerms
+end
+
+exports('checkForRole', checkForRole)
+
+getRoles = function()
+    local roles = lib.callback.await('ws_discord:getRoles', 100)
+    if roles then
+        return roles
+    else
+        return false
+    end
+end
+
+exports('checkForRole', checkForRole)
