8b63520d123db93a0a851bc7202a7351e5364f65
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-10-04 10:39:12 -0700

    chore: formatting

--- server/server.lua
+++ server/server.lua
@@ -105,7 +105,7 @@ end)
 
 -- Functions
 
-function copyTable(obj, seen)
+copyTable = function(obj, seen)
     if type(obj) ~= 'table' then return obj end
     if seen and seen[obj] then return seen[obj] end
     local s = seen or {}
