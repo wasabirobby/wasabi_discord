5fac622d347a6ce89ea6f6e7c9097e7bb08c652b
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-10-03 22:53:49 -0700

    chore: name change

--- client/functions.lua
+++ client/functions.lua
@@ -3,14 +3,14 @@
 ---------------------------------------------------------------
 
 checkForRole = function(roleId)
-    local hasPerms = lib.callback.await('ws_discord:checkForRole', 100, roleId)
+    local hasPerms = lib.callback.await('wasabi_discord:checkForRole', 100, roleId)
     return hasPerms
 end
 
 exports('checkForRole', checkForRole)
 
 getRoles = function()
-    local roles = lib.callback.await('ws_discord:getRoles', 100)
+    local roles = lib.callback.await('wasabi_discord:getRoles', 100)
     if roles then
         return roles
     else
